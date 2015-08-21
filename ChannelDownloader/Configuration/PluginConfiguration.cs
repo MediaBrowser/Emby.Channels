7db1bb04f60f55fb41cb46ebad654b5f036d267d
Luke Pulverenti <luke.pulverenti@gmail.com>
2015-08-21 13:06:37 -0400

    add channel downloading plugin

---
+++ ChannelDownloader/Configuration/PluginConfiguration.cs
@@ -0,0 +1,22 @@
+﻿using MediaBrowser.Model.Plugins;
+
+namespace ChannelDownloader.Configuration
+{
+    /// <summary>
+    /// Class PluginConfiguration
+    /// </summary>
+    public class PluginConfiguration : BasePluginConfiguration
+    {
+        public string DownloadPath { get; set; }
+        public int? MaxDownloadAge { get; set; }
+        public string[] DownloadingChannels { get; set; }
+        public double? DownloadSizeLimit { get; set; }
+
+        public PluginConfiguration()
+        {
+            DownloadingChannels = new string[] { };
+            DownloadSizeLimit = .5;
+            MaxDownloadAge = 30;
+        }
+    }
+}
---
+++ ChannelDownloader/Plugin.cs
@@ -0,0 +1,52 @@
+﻿using ChannelDownloader.Configuration;
+using MediaBrowser.Common.Configuration;
+using MediaBrowser.Common.Plugins;
+using MediaBrowser.Model.Serialization;
+using System.Threading;
+
+namespace ChannelDownloader
+{
+    /// <summary>
+    /// Class Plugin
+    /// </summary>
+    public class Plugin : BasePlugin<PluginConfiguration>
+    {
+        /// <summary>
+        /// Apple doesn't seem to like too many simulataneous requests.
+        /// </summary>
+        public readonly SemaphoreSlim AppleTrailers = new SemaphoreSlim(1, 1);
+
+        public Plugin(IApplicationPaths applicationPaths, IXmlSerializer xmlSerializer)
+            : base(applicationPaths, xmlSerializer)
+        {
+            Instance = this;
+        }
+
+        /// <summary>
+        /// Gets the name of the plugin
+        /// </summary>
+        /// <value>The name.</value>
+        public override string Name
+        {
+            get { return "Channel Downloader"; }
+        }
+
+        /// <summary>
+        /// Gets the description.
+        /// </summary>
+        /// <value>The description.</value>
+        public override string Description
+        {
+            get
+            {
+                return "Download channel content in advance.";
+            }
+        }
+
+        /// <summary>
+        /// Gets the instance.
+        /// </summary>
+        /// <value>The instance.</value>
+        public static Plugin Instance { get; private set; }
+    }
+}
